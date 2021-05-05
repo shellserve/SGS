1851e81008e7bcb39c482adbb32eb323b0b88b47
NicholasWood <nicholaswood004@gmail.com>
2021-05-05 14:23:39 -0400

    Cleaned Models. Began plan implementation.

--- sgs_api/models/project_model.py
+++ sgs_api/models/project_model.py
@@ -2,18 +2,18 @@ from sgs_api.database import db
 
 class ProjectModel(db.Model):
     #if you know sql this is very similar to that
-    __tablename__ = 'project'
+    __tablename__ = 'projects'
     project_id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
     name = db.Column(db.String(100))
     objective = db.Column(db.String(1000))
     
     #__repr__ can be called just to get a quick glance at the model variables
     def __repr__(self):
         return (
-            f"***Project***"
-            f"project_id: {self.project_id}"
-            f"owner_id: {self.owner_id}"
-            f"name: {self.name}"
-            f"objective: {self.objective}"
+            f"***Project***\n"
+            f"project_id: {self.project_id}\n"
+            f"owner_id: {self.owner_id}\n"
+            f"name: {self.name}\n"
+            f"objective: {self.objective}\n"
             )
